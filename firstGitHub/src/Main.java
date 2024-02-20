ea0d008eafacbff2b45b5f098cf2c5381c19dbcb
PriyumP <126Ppatel@frhsd.com>
2024-02-20 12:40:31 -0500

    Revert "age"
    This reverts commit 098a2283e5fd18a33fd86cf8df822805203a0c06.

--- firstGitHub/src/Main.java
+++ firstGitHub/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello Priyum!");
-
-        for(int i = 0; i < 16; i++){
-            System.out.println("I am " + i + " years old.");
-        }
     }
 }
